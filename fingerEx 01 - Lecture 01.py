db8da43f5e5c54f720458a2ed0f265101cb412db
flpcoding <flp.coding@gmai.com>
2025-01-01 20:15:08 -0300

    Aula 01

---
+++ fingerEx 01 - Lecture 01.py
@@ -0,0 +1,8 @@
+# Declaração de variáveis.
+a = 1
+b = 2
+c = 3
+# Operadores aritméticos.
+total = (a + b) * c
+# print() - Exibe o valor ou mensagem passado como parâmetro no console.
+print(total)
